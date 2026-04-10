049a25825431b1757d653793ec6724ce0944e001
yoonhj7173 <yoonhj7173@gmail.com>
2026-04-10 14:11:49 +0900

    initial commit

---
+++ src/main/java/com/back/storymate/StorymateApplication.java
@@ -0,0 +1,13 @@
+package com.back.storymate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorymateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorymateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/storymate/StorymateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.storymate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorymateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
