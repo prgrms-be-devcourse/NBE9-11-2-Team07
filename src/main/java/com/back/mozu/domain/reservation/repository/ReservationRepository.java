ff2349b763dfda8489d164389f5b4d94c208ea91
강경서 <rudtj2306@ajou.ac.kr>
2026-04-15 00:25:08 +0900

    feat: 대기열 복구 API 구현

--- src/main/java/com/back/mozu/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/back/mozu/domain/reservation/repository/ReservationRepository.java
@@ -1,4 +1,11 @@
 package com.back.mozu.domain.reservation.repository;
 
-public interface ReservationRepository {
+import com.back.mozu.domain.reservation.entity.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ReservationRepository extends JpaRepository<Reservation, UUID> {
+    Optional<Reservation> findByCustomerIdAndStatus(UUID customerId, String status);
 }
