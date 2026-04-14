c19713622e551205d66f8aa61ed496a7ebbc7e9c
5s-mini <dnclsehd123@naver.com>
2026-04-14 14:52:39 +0900

    refactor: 주석 추가

--- src/main/java/com/back/mozu/domain/queue/dto/QueueDto.java
+++ src/main/java/com/back/mozu/domain/queue/dto/QueueDto.java
@@ -5,22 +5,27 @@ import java.util.UUID;
 
 public class QueueDto {
 
+    // 예약 시도 요청 객체
     public static class AttemptRequest {
-        private UUID timeSlotId;
-        private int guestCount;
+        private UUID timeSlotId; // 타임슬롯 고유 ID
+        private int guestCount; // 방문 인원수
 
+        // Json 기본 생성자
         protected AttemptRequest() {
         }
 
+        // 데이터 전달을 위한 객체 (타임슬롯 고유 ID)
         public UUID getTimeSlotId() {
             return timeSlotId;
         }
 
+        // 데이터 전달을 위한 객체 (방문 인원수)
         public int getGuestCount() {
             return guestCount;
         }
     }
 
+    // 예약 시도 성공 시 응답 객체
     public static class AttemptResponse {
         private UUID attemptId;
 
@@ -33,6 +38,7 @@ public class QueueDto {
         }
     }
 
+    // 현재 처리 상태 객체
     public static class StatusResponse {
         private ReservationStatus status;
 
