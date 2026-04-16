f8949d1ff69e78e84f4772302ece93e9794e1c24
unknown <rkdchl31501@gmail.com>
2026-04-16 09:47:16 +0900

    feat: 현재 로그인 유저 조회 API 구현

--- src/main/java/com/back/mozu/domain/customer/controller/ApiV1AuthController.java
+++ src/main/java/com/back/mozu/domain/customer/controller/ApiV1AuthController.java
@@ -2,7 +2,9 @@ package com.back.mozu.domain.customer.controller;
 
 import com.back.mozu.domain.customer.dto.CustomerDto;
 import com.back.mozu.domain.customer.service.AuthController;
+import com.back.mozu.global.response.RsData;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +18,13 @@ public class ApiV1AuthController {
     private final AuthController authController;
 
     @GetMapping("/me")
-    public CustomerDto.MeResponse getMe(
+    public ResponseEntity<RsData<CustomerDto.MeResponse>> getMe(
             @RequestHeader("X-USER-EMAIL") String email
     ) {
-        return authController.getMe(email);
+        CustomerDto.MeResponse response = authController.getMe(email);
+
+        return ResponseEntity.ok(
+                RsData.of("200", "유저 정보 조회에 성공했습니다.", response)
+        );
     }
 }
--- src/main/java/com/back/mozu/global/response/RsData.java
+++ src/main/java/com/back/mozu/global/response/RsData.java
@@ -1,4 +1,18 @@
 package com.back.mozu.global.response;
 
-public class RsData {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public record RsData<T>(
+        String msg,
+        String resultCode,
+        T data
+) {
+    public static <T> RsData<T> of(String resultCode, String msg, T data) {
+        return new RsData<>(msg, resultCode, data);
+    }
+
+    @JsonIgnore
+    public int getStatusCode() {
+        return Integer.parseInt(resultCode.split("-")[0]);
+    }
 }
