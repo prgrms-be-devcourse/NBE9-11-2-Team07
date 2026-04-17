8c8b37909e367c7cccee052989fed54977c5fe94
yoonhj7173 <yoonhj7173@gmail.com>
2026-04-17 11:31:03 +0900

    Reservation 도메인 통합 완료 및 auth 중간 확인 완료.

--- src/main/java/com/back/mozu/domain/customer/controller/ApiV1AuthController.java
+++ src/main/java/com/back/mozu/domain/customer/controller/ApiV1AuthController.java
@@ -1,7 +1,7 @@
 package com.back.mozu.domain.customer.controller;
 
 import com.back.mozu.domain.customer.dto.CustomerDto;
-import com.back.mozu.domain.customer.service.AuthController;
+import com.back.mozu.domain.customer.service.AuthService;
 import com.back.mozu.global.response.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,13 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/auth")
 public class ApiV1AuthController {
 
-    private final AuthController authController;
+    private final AuthService authService;
 
     @GetMapping("/me")
     public ResponseEntity<RsData<CustomerDto.MeResponse>> getMe(
             @RequestHeader("X-USER-EMAIL") String email
     ) {
-        CustomerDto.MeResponse response = authController.getMe(email);
+        CustomerDto.MeResponse response = authService.getMe(email);
 
         return ResponseEntity.ok(
                 RsData.of("200", "유저 정보 조회에 성공했습니다.", response)
--- src/main/java/com/back/mozu/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/back/mozu/domain/reservation/repository/ReservationRepository.java
@@ -3,10 +3,14 @@ package com.back.mozu.domain.reservation.repository;
 import com.back.mozu.domain.reservation.entity.Reservation;
 import com.back.mozu.domain.reservation.entity.ReservationStatus;
 import com.back.mozu.domain.reservation.entity.TimeSlot;
-import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
 
 // 예약 CRUD 작업
 public interface ReservationRepository extends JpaRepository<Reservation, UUID> {
     boolean existsByUserIdAndTimeSlotAndStatusNot(UUID customerId, TimeSlot timeSlot, ReservationStatus status);
+
+    List<Reservation> findAllByCustomerId(UUID customerId);
 }
