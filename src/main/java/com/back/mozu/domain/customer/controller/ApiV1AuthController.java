76e5184bcfab4382692e7a86212f8cfe2289f43f
unknown <rkdchl31501@gmail.com>
2026-04-17 11:46:17 +0900

    refactor: auth me에 Rq 적용

--- src/main/java/com/back/mozu/domain/customer/controller/ApiV1AuthController.java
+++ src/main/java/com/back/mozu/domain/customer/controller/ApiV1AuthController.java
@@ -2,11 +2,11 @@ package com.back.mozu.domain.customer.controller;
 
 import com.back.mozu.domain.customer.dto.CustomerDto;
 import com.back.mozu.domain.customer.service.AuthController;
+import com.back.mozu.global.response.Rq;
 import com.back.mozu.global.response.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,11 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiV1AuthController {
 
     private final AuthController authController;
+    private final Rq rq;
 
     @GetMapping("/me")
-    public ResponseEntity<RsData<CustomerDto.MeResponse>> getMe(
-            @RequestHeader("X-USER-EMAIL") String email
-    ) {
+    public ResponseEntity<RsData<CustomerDto.MeResponse>> getMe() {
+        String email = rq.getCurrentUserEmail();
         CustomerDto.MeResponse response = authController.getMe(email);
 
         return ResponseEntity.ok(
