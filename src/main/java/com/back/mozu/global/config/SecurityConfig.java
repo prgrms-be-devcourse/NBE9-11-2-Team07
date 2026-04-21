a92e257cef08666b613183a0c66ae9c6ca48f672
강경서 <rudtj2306@ajou.ac.kr>
2026-04-21 15:21:48 +0900

    feat: actuator 보안 설정 및 grafana 볼륨 추가

--- src/main/java/com/back/mozu/global/config/SecurityConfig.java
+++ src/main/java/com/back/mozu/global/config/SecurityConfig.java
@@ -99,7 +99,8 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/swagger-ui.html",
                                 "/swagger-ui/**",
-                                "/v3/api-docs/**"
+                                "/v3/api-docs/**",
+                                "/actuator/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
