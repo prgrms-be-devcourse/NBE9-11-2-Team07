9e62fffb3ba1ac8aed3897363ed0b9b96648b301
kyoungtta <150694971+kyoungtta@users.noreply.github.com>
2026-04-23 14:25:23 +0900

    feat: 동적 스케줄링으로 랜덤반환 처리 + 정적/동적 스케줄러 테스트 추가 (#41)
    * feat: 동적 스케줄링으로 랜덤반환 처리 + 정적/동적 스케줄러 테스트 추가
    
    * feat:낙관적락 동시성제어테스트
    
    ---------
    
    Co-authored-by: 강경서 <rudtj2306@ajou.ac.kr>

--- src/main/java/com/back/mozu/domain/reservation/service/StaticReleaseScheduler.java
+++ src/main/java/com/back/mozu/domain/reservation/service/StaticReleaseScheduler.java
@@ -14,7 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Primary
+
 @Component
 @RequiredArgsConstructor
 public class StaticReleaseScheduler implements ReleaseScheduler {
